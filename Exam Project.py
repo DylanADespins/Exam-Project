5b8667b2c3ce5ae36b596020f9ac3ca95028cdfe
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-03-07 09:56:55 -0500

    Added 1st story and Age

--- Exam Project.py
+++ Exam Project.py
@@ -1,2 +1,9 @@
 # Dylan Despins
 # 3/7/19
+
+print('A elfish man, in the medieval times where dragons and wizards still existed, was forced to join the Knights of the Blood Oath.')
+print('The same elfish man, only a young child now, adventured the lands of Termina.')
+
+
+elfish_child_age = 12
+elfish_man_age = 17
