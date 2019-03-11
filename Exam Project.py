29e51183c7fc60d2ac6a56b5e8efafeb50f4623c
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-03-11 09:48:50 -0400

    Merged Master and try-and-ifelseelif

--- Exam Project.py
+++ Exam Project.py
@@ -5,5 +5,17 @@ print('A elfish man, in the medieval times where dragons and wizards still exist
 print('The same elfish man, only a young child now, adventured the lands of Termina.')
 
 
-elfish_child_age = 12
-elfish_man_age = 17
+
+
+try:
+    who_will_you_follow = int(input("\n" "Who will you follow? The 17 year old Elfish Man(1) or the 12 year old Elfish Child(2)."))
+    if who_will_you_follow == (1):
+        print("Your Choice: " + str(who_will_you_follow) + " (Elfish Man)")
+        print("The Young Elfish Man was sent out to fight off a dragon")
+
+    if who_will_you_follow == (2):
+        print("Your Choice: " + str(who_will_you_follow) + " (Elfish Child)")
+        print("The Young Elfish Child was saving the village of Tarnace from hundreds of thieves and bandits attacking after walking the dangerous lands of Termina.")
+except ValueError:
+    print("Try (1) or (2)")
+
