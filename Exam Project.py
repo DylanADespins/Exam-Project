6a998b8fdb0b550df69f2be6bb2a81028a448b28
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-03-13 09:56:50 -0400

    Finished while loops and 1st story line

--- Exam Project.py
+++ Exam Project.py
@@ -1,11 +1,18 @@
 # Dylan Despins
 # 3/7/19
 
+name = input("What is your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you!")
+
+greeting()
 
 '''
 The first part of the story starts here
 '''
-print('A elfish man, in the medieval times where dragons and wizards still existed, was forced to join the Knights of the Blood Oath.')
+print('An elfish man, in the medieval times where dragons and wizards still existed, was forced to join the Knights of the Blood Oath.')
 print('The same elfish man, only a young child now, adventured the lands of Termina.')
 
 
@@ -25,13 +32,15 @@ try:
         print("Your Choice: " + str(who_will_you_follow) + " (Elfish Man)")
         print("The Young Elfish Man was sent out to fight off a dragon as a duty for the Kingdom")
 # Elfish Child story
+
+
     if who_will_you_follow == (2):
         print("Your Choice: " + str(who_will_you_follow) + " (Elfish Child)")
         print("The Young Elfish Child was saving the village of Tarnace from hundreds of thieves and bandits attacking after walking the dangerous lands of Termina.")
 
 except ValueError:
     print("Use one of the two numbers")
-
+#If in the beginning of the program the user types in the number 1 he will see this part of the story and then he will decide if he wants to fight or tame a dragon.
 while who_will_you_follow == 1:
     print("\n" "The Elfish Man travels up the mountain to find the nest of the dragon and finds that it's just a baby fire dragon ")
     your_own_pet = (input("\n""While it is a baby it still burnt down an entire village. Will you take it in with you on your adventures? (yes/no)"))
@@ -43,16 +52,19 @@ while who_will_you_follow == 1:
         if the_right_choice == ("yes"):
             print("\n"" You move in to slash it with your sword and its mother appears deeper in the cave, and you now understand this is not a mission for you to complete")
             print("\n""You are now known as dead in the kingdom you once served as you never returned from the mission and live in the forest and are one with the animals")
-
+            print("Later in life, the elfish man will start a family and make a community of his own where there are shops, blacksmithes, restaurants, bars, etc. in the area.")
+            break
+if who_will_you_follow == 1:
+    print("Thank you for reading the story of an Elfish Man that was forced into working for the kingdom and only wanted to live in harmony.")
 while who_will_you_follow == 2:
     print("\n""The Elfish Child had been defending the village from the bandits for a few hours and finally the action died down.")
-    print("\n""The village offers you a gift for protecting them from the bandits, but they have you pick from two chests and you don't know what is in either one of them, all you know is that they both have a item of value in it.))
-    the_reward = input("Which chest will you choose? The chest to the left or the chest to the right, some reason the villagers are more directly pointing toward the one to the right.(r/l)")
-    if the_reward == r:
+    print("\n""The village offers you a gift for protecting them from the bandits, but they have you pick from two chests and you don't know what is in either one of them, all you know is that they both have a item of value in it.")
+    the_reward = (input("Which chest will you choose? The chest to the left or the chest to the right, some reason the villagers are more directly pointing toward the one to the right.(r/l)"))
+    if the_reward == ("r"):
           print ("\n""The villagers rewarded you with something of value from all of the villagers, a village of blacksmithes and shopkeepers gave you a mighty sword that they had spent days making before the raid of the bandits")
           print ("\n""This sword gave a very mysterious presence, you slash it around in a field of grass and started to speak to you.")
           break
-    if the_reward == l:
+    if the_reward == ("l"):
           print ("\n"" The villagers rewarded you with a lot of cash, a village of blacksmithes and shopkeepers, you assume the other chest had a sword of some sorts or something.")
           print ("\n"" You still feel like you did the right thing, but the other chest will always be on your mind from here on")
           break
